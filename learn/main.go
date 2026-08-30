8145ee90b66115d34f6e86c6da50715e23fc6c32
dbotelho <diogolbsilva2006@gmail.com>
2026-08-30 20:52:22 +0100

    this shit is ez

--- learn/main.go
+++ learn/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"time")
 
 func main() {
-	fmt.Println("Hello world!")
+	var c, python = "c", "python"
+	var integers int = 1
+	var floats float64 = 67.69
+	var boolean bool = true
+	var strings string = "The python with C speed?"
+
+	fmt.Println("=== Standard types ===\n")
+	fmt.Printf(" integers: %v \n floats: %v \n boolean: %v \n strings: %q\n", integers, floats, boolean, strings)
+
+	fmt.Println("\nHello world!")
+	fmt.Println("The time is", time.Now())
 }
